48d0d11f2a50e1f8b0409a97a64635f9f634fed4
Azat Minvaliev <Miatozoff@gmail.com>
2015-01-26 16:17:53 +0200

    Initial Commit

---
+++ FlickrViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrViewer
+//
+//  Created by Азат on 26.01.15.
+//  Copyright (c) 2015 Azat Minvaliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
